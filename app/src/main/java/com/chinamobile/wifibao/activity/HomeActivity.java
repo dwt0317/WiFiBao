f63964b08329b5f2b323ec540dac470c9c3297cf
ping <wangyp828@126.com>
2016-03-31 17:04:54 +0800

    layout

--- app/src/main/java/com/chinamobile/wifibao/activity/HomeActivity.java
+++ app/src/main/java/com/chinamobile/wifibao/activity/HomeActivity.java
@@ -3,6 +3,7 @@ package com.chinamobile.wifibao.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.drawable.BitmapDrawable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -96,5 +97,24 @@ public class HomeActivity extends Activity{
                 }
             }
         });
+    }
+
+    private long exitTime = 0;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exit();
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    public void exit() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序",Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            finish();
+            System.exit(0);
+        }
     }
 }
