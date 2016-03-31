0c8e876a151e479534a07e7bbbde0a4dd86382e0
chendongdong123 <chendongdong_yjy@163.com>
2016-03-31 21:51:21 +0800

    修改ShareActivity，新建获取mac地址的方法，修改提示信息

--- app/src/main/java/com/chinamobile/wifibao/activity/ShareActivity.java
+++ app/src/main/java/com/chinamobile/wifibao/activity/ShareActivity.java
@@ -2,6 +2,7 @@ package com.chinamobile.wifibao.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.chinamobile.wifibao.R;
+import com.chinamobile.wifibao.bean.WiFi;
+import com.chinamobile.wifibao.utils.DatabaseUtil;
 import com.chinamobile.wifibao.utils.WifiApAdmin;
 
 import java.lang.reflect.Method;
@@ -18,8 +21,6 @@ import java.lang.reflect.Method;
 public class ShareActivity extends Activity {
     private Context mContext = null;
     private static final String METHOD_GET_WIFI_AP_STATE = "getWifiApState";
-    private static final String METHOD_SET_WIFI_AP_ENABLED = "setWifiApEnabled";
-    private static final String METHOD_GET_WIFI_AP_CONFIG = "getWifiApConfiguration";
     private static final String METHOD_IS_WIFI_AP_ENABLED = "isWifiApEnabled";
 
     @Override
@@ -34,21 +35,29 @@ public class ShareActivity extends Activity {
             public void onClick(View v){
                 String name=((EditText)findViewById(R.id.apnametext)).getText().toString().trim();
                 String password=((EditText)findViewById(R.id.passwordtext)).getText().toString().trim();
+                String share = ((EditText) findViewById(R.id.maxsharetext)).getText().toString().trim();
+                String access = ((EditText) findViewById(R.id.maxaccesstext)).getText().toString().trim();
                 if(name.isEmpty() || password.isEmpty()){
-                    Toast.makeText(getApplicationContext(),"wifi名称或者密码不能为空！",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"wifi名称或者密码不能为空！",Toast.LENGTH_SHORT).show();
                 }
                 else if(password.length() < 8 ){
-                    Toast.makeText(getApplicationContext(),"密码长度不能小于8！",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"密码长度不能小于8！",Toast.LENGTH_SHORT).show();
                 }
                 else {
                     WifiApAdmin wifiAp = new WifiApAdmin(mContext);
                     wifiAp.startWifiAp(name, password);
-                    Toast.makeText(mContext,"努力开启中...",Toast.LENGTH_LONG).show();
-                    //跳转到开启ap成功的页面，下个页面可以关闭ap
+                    Toast.makeText(mContext, "宝宝努力开启中...", Toast.LENGTH_LONG).show();
+                    WiFi ap = new WiFi();
+                    ap.setSSID(name);
+                    ap.setPassword(password);
+                    ap.setUpperLimit(Double.parseDouble(share));//没有判断非法输入
+                    ap.setMaxConnect(Integer.parseInt(access));
+                    ap.setBSSID(getLocalMacAddress());
+                    //DatabaseUtil.writeApToDatabase(mContext, ap);
+
                     Intent intent = new Intent(ShareActivity.this, CloseApActivity.class);
                     startActivity(intent);
                     overridePendingTransition(R.anim.scale_in, R.anim.alpha_out);
-
                 }
             }
         });
@@ -71,4 +80,13 @@ public class ShareActivity extends Activity {
         }
     }
 
+    /***
+     * 获取MAC地址
+     * @return
+     */
+    private String getLocalMacAddress() {
+        WifiManager wifi = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        return info.getMacAddress();
+    }
 }
