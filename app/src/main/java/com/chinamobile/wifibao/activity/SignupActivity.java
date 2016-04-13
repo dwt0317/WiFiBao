f1db38cf21527b789a28029db0682b9d4378574d
chen_git <meitianmeitian163@163.com>
2016-04-13 16:10:31 +0800

    添加注册页面Activity

--- app/src/main/java/com/chinamobile/wifibao/activity/SignupActivity.java
+++ app/src/main/java/com/chinamobile/wifibao/activity/SignupActivity.java
@@ -19,7 +19,7 @@ public class SignupActivity extends AppCompatActivity {
     private EditText phonenumber;
     private TextView username;
     private EditText password;
-    private EditText repwd;
+    private EditText repassword;
     private EditText veriCode;
     private Button register;
 
@@ -36,16 +36,18 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void setViewComponent() {
-        phonenumber = (EditText) findViewById(R.id.phonenumber);
-        username = (TextView) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.RegPwd);
-        repwd = (EditText) findViewById(R.id.RegPwdAgain);
-        veriCode = (EditText) findViewById(R.id.RegMail);
-        register = (Button) findViewById(R.id.register);
+        phonenumber = (EditText)findViewById(R.id.phonenumber);
+        username = (TextView)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.RegPwd);
+        repassword = (EditText)findViewById(R.id.RegPwdAgain);
+        veriCode = (EditText)findViewById(R.id.RegMail);
+        register = (Button)findViewById(R.id.register);
 
         password.setInputType(InputType.TYPE_CLASS_TEXT
                 | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
+        repassword.setInputType(InputType.TYPE_CLASS_TEXT
+                | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,7 +61,7 @@ public class SignupActivity extends AppCompatActivity {
         phonenumberStr = phonenumber.getText().toString().trim();
         usernameStr = username.getText().toString().trim();
         passwordStr = password.getText().toString().trim();
-        rePwdStr = repwd.getText().toString().trim();
+        rePwdStr = repassword.getText().toString().trim();
         veriCodeStr = veriCode.getText().toString();
 
 
@@ -73,6 +75,7 @@ public class SignupActivity extends AppCompatActivity {
             phonenumber.setError("请输入正确的手机号");
             return false;
         }
+
         if (usernameStr == null || usernameStr.equals("")) {
             username.setError("昵称不能为空");
             return false;
@@ -83,12 +86,12 @@ public class SignupActivity extends AppCompatActivity {
             return false;
         }
         if (rePwdStr == null || usernameStr.equals("")) {
-            password.setError("请再次输入密码");
+            repassword.setError("请再次输入密码");
             return false;
         }
 
         if (!rePwdStr.equals(passwordStr)) {
-            repwd.setError("两次输入密码不一致");
+            repassword.setError("两次输入密码不一致");
             return false;
         }
 
