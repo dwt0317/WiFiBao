8e13c25fe45411c79bb5b172730994650b9894e6
chendongdong123 <chendongdong_yjy@163.com>
2016-04-17 23:04:37 +0800

    热点分享--修改同步数据的操作--false/true--修复bug

--- app/src/main/java/com/chinamobile/wifibao/utils/DatabaseUtil.java
+++ app/src/main/java/com/chinamobile/wifibao/utils/DatabaseUtil.java
@@ -44,7 +44,7 @@ public class DatabaseUtil {
         BmobQuery<WiFi> query = new BmobQuery<WiFi>();
 
         query.addWhereEqualTo("BSSID", wifiAp.getBSSID());
-        query.addWhereEqualTo("SSID", wifiAp.getBSSID());
+        query.addWhereEqualTo("SSID", wifiAp.getSSID());
         query.addWhereEqualTo("password", wifiAp.getPassword());
         query.addWhereEqualTo("upperLimit", wifiAp.getUpperLimit());
         query.addWhereEqualTo("maxConnect", wifiAp.getMaxConnect());
@@ -53,11 +53,33 @@ public class DatabaseUtil {
 
         query.findObjects(context, new FindListener<WiFi>() {
             @Override
-            public void onSuccess(List<WiFi> object) {
-                Log.i("DatabaseU obj: ",String.valueOf(object.size()));
+            public void onSuccess(final List<WiFi> object) {
+                final int len = object.size();
+                Log.i("DatabaseU obj: ",String.valueOf(len));
+                if(len == 0){
+                    wifiAp.save(context, new SaveListener() {
+                        @Override
+                        public void onSuccess() {
+                            apObjectId = wifiAp.getObjectId();
+                            Log.i("DatabaseUtil添加数据成功,", apObjectId);
+                            Message message = new Message();
+                            message.arg1 = 1;
+                            handler.sendMessage(message);
+                        }
+
+                        @Override
+                        public void onFailure(int code, String arg0) {
+                            Log.i("DatabaseUtil error,", arg0);
+                            Message message = new Message();
+                            message.arg1 = 0;
+                            handler.sendMessage(message);
+                        }
+                    });
+                    return;
+                }
                 final Message message = new Message();
                 message.what=0;
-                for (WiFi obj : object) {
+                for (final WiFi obj : object) {
                     //修改状态
                     obj.setState(true);
                     obj.update(context,obj.getObjectId(), new UpdateListener() {
@@ -65,47 +87,39 @@ public class DatabaseUtil {
                         public void onSuccess() {
                             Log.i("update succ", "!");
                             message.what+=1;
+                            //异步，不要再外面判断
+                            if(message.what == len){
+                                Log.i("message what: ",String.valueOf(message.what));
+                                message.arg1 = 1;
+                                handler.sendMessage(message);
+                            }else if(obj.equals(object.get(len-1))){
+                                Log.i("message what: ",String.valueOf(message.what));
+                                message.arg1 = 0;
+                                handler.sendMessage(message);
+                            }
+
                         }
 
                         @Override
                         public void onFailure(int i, String s) {
                             Log.i("update erro", s);
+                            Message message = new Message();
+                            message.arg1 = 0;
+                            handler.sendMessage(message);
 
                         }
                     });
                 }
-                if(message.what == object.size()){
-                    message.arg1 = 1;
-                }else{
-                    message.arg1 = 0;
-                }
-                handler.sendMessage(message);
+                //异步，不要再外面判断
 
             }
 
             @Override
             public void onError(int code, String msg) {
-                Log.i("查询失败，执行插入：", msg);
-
-                //wifiAp.setState(true);
-                wifiAp.save(context, new SaveListener() {
-                    @Override
-                    public void onSuccess() {
-                        apObjectId = wifiAp.getObjectId();
-                        Log.i("DatabaseUtil添加数据成功,", apObjectId);
-                        Message message = new Message();
-                        message.arg1 = 1;
-                        handler.sendMessage(message);
-                    }
-
-                    @Override
-                    public void onFailure(int code, String arg0) {
-                        Log.i("DatabaseUtil error,", "添加数据失败!!!!");
-                        Message message = new Message();
-                        message.arg1 = 0;
-                        handler.sendMessage(message);
-                    }
-                });
+                Log.i("查询失败", msg);// The network is not available,please check your network!
+                Message message = new Message();
+                message.arg1 = 0;
+                handler.sendMessage(message);
             }
         });
 
