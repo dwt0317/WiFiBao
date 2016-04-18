817175086f20ca06a3b691a73595295d9914cb52
chendongdong123 <chendongdong_yjy@163.com>
2016-04-18 16:07:15 +0800

    简单的热点评级工具

---
+++ app/src/main/java/com/chinamobile/wifibao/utils/WifiGradeUtil.java
@@ -0,0 +1,16 @@
+package com.chinamobile.wifibao.utils;
+
+import com.chinamobile.wifibao.bean.WiFi;
+
+/**
+ * Created by cdd on 2016/4/18.
+ */
+public class WifiGradeUtil {
+    public static double getGrade(WiFi wifi){
+        int maxConnect = wifi.getMaxConnect();
+        double maxShare = wifi.getUpperLimit();
+        double grade = maxShare*1.5/maxConnect;
+        return grade;
+    }
+
+}
