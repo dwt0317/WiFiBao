aa3b307f51c0a21b8333b75f73362f8184f6e86b
dwt0317 <469621636@qq.com>
2016-05-06 00:05:26 +0800

    修改connectionPool

--- app/src/main/java/com/chinamobile/wifibao/bean/ConnectionPool.java
+++ app/src/main/java/com/chinamobile/wifibao/bean/ConnectionPool.java
@@ -10,7 +10,7 @@ public class ConnectionPool extends BmobObject {
     private WiFi WiFi;
     private Double cost;
     private Double flowUsed;
-    private String username;
+    private User user;
     private Integer maxConnect;
     private Integer curConnect;
 
@@ -38,6 +38,13 @@ public class ConnectionPool extends BmobObject {
         this.flowUsed = flowUsed;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Integer getMaxConnect() {
         return maxConnect;
@@ -53,13 +60,5 @@ public class ConnectionPool extends BmobObject {
 
     public void setCurConnect(Integer curConnect) {
         this.curConnect = curConnect;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 }
